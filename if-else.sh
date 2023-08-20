0db37beaf71531a06571c7ab28714ec2fb954909
psaikumar <psaikumar586@gmail.com>
2023-08-21 00:29:14 +0530

    changes variable2

--- if-else.sh
+++ if-else.sh
@@ -3,10 +3,10 @@
 a=$1
 b=$2
 
-if($a -ge $b) 
-echo "a grater than b : $a"
+if($a == $b) 
+echo "a eq than b "
 else 
-echo "b is greater $a"
+echo "b is ne "
 fi
 
 # a=5, b=2 a>b 5>2 a is grtr b 
