934c46e99dddaceb7cdb836b81cda2c510783eaf
psaikumar <psaikumar586@gmail.com>
2023-08-20 20:51:24 +0530

    changes variable2

--- variable2.sh
+++ variable2.sh
@@ -1,8 +1,13 @@
  #!/bin/bash
 
-var1=$1
-var2=$2
+# var1=$1
+# var2=$2
 
-sum=$(var1+var2)
-echo " total value is : $sum"
+# sum=$(var1+var2)
+# echo " total value is : $sum"
 
+$1 = "ram"
+$2 = "sai"
+
+echo "$1 is good boy"
+echo "$2 is v.good boy"
