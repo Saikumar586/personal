0811dc4d1fa6c149e901e6871c00fae40235012b
psaikumar <psaikumar586@gmail.com>
2023-08-21 22:58:10 +0530

    conditions1

---
+++ conditions1.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+USER=$(id -u)
+BGreen='\033[1;32m'
+Red='\033[0;31'
+
+if [ $USER -ne 0 ]
+then
+    echo "its not root user "
+    exit 1
+ #else 
+ #echo "  login with root user"
+fi
+    yum install nginx -y
+
+if [ $? -ne 0 ]
+then
+    echo "installation error"
+    exit 1
+else
+    echo -e "\e installation ${BGreen} success..! \e[m"
+fi
+
+yum install postfix -y
+
+if [ $? -ne 0 ]
+then
+    echo -e "\e postfix ${Red} error \e[m"
+    exit 1
+else 
+    echo -e "\e postfix ${BGreen} installed \e[m]"
+    exit 0
+fi
