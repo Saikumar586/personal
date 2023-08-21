fb2c5afbde8ef4709410d01fcc57a69c4676c169
psaikumar <psaikumar586@gmail.com>
2023-08-21 17:37:50 +0530

    changes conditions

--- conditions2.sh
+++ conditions2.sh
@@ -9,7 +9,7 @@
 
 USER=$(id -u)
 BGreen='\033[1;32m'
-_RED=$(tput setaf 1)
+Red="\033[0;31'
 
 if [ $USER -ne 0 ]
 then
@@ -29,9 +29,10 @@ else
 fi
 
 yum install postfeix -y
+
 if [ $? -ne 0 ]
 then
-    echo "\e ${_red}postfix error \e[m"
+    echo "\e ${Red}postfix error \e[m"
     exit 1
 else 
     echo "\e ${BGreen}postfix installed \e[m"
