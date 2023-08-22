6c113d562386fe3f4625c12c5e273283da04f834
psaikumar <psaikumar586@gmail.com>
2023-08-22 23:36:54 +0530

    functions

---
+++ loops.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# USERID=[ id -u ]
+
+# VALIDATE(){
+
+#     if [ USERID -ne 0 ]
+#     then
+#     echo "login with root user"
+#     else
+#     echo "install server..."
+# }
+
+# for install in {$nginx..$postfix}
+# do 
+#     echo "install server:$nginx,$postfix"
+# done
+
+for i in list {1..100}
+do 
+echo i
+done
