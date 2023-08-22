6c58c9b22a04c98dcdba3e2a65c5cd3bd21bd342
psaikumar <psaikumar586@gmail.com>
2023-08-23 01:04:25 +0530

    functions

--- loops.sh
+++ loops.sh
@@ -42,7 +42,9 @@ fi
 
 for i in $@
 do 
-    yum install $i -q list installed $i &>/dev/null && echo "Installed" || echo "Not installed" &>>$LOGFILE
+    yum install $i &>>$LOGFILE
+    
+    yum -q list installed $i &>/dev/null && echo "Installed" || echo "Not installed" &>>$LOGFILE
 
     #yum install $i &>>$LOGFILE
 
