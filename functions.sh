778b0b1be2d8ab168459d8ece395993627f6ab12
psaikumar <psaikumar586@gmail.com>
2023-08-22 20:06:32 +0530

    functions

--- functions.sh
+++ functions.sh
@@ -3,22 +3,22 @@
 USERID=$(id -u)
 BGreen='\e[0;32m'
 Red='\e[1;31m'
-# VALIDATE(){
-# if [ $1 -ne 0 ]
-# then 
-#     echo -e "\e $2 ${Red} failure \e[m "
-#     exit 1
-# else
-#     echo -e "\e $2 ${BGreen} success \e[m "
-# fi
-VALIDATE() {
-if [ $1 -ne 0 ];
-then
-echo -e "$2${Red} is failure \e[m"
+VALIDATE(){
+if [ $1 -ne 0 ]
+then 
+    echo -e "$2 ${Red} failure \e[m "
+    exit 1
 else
-echo -e "$2${BGreen} is success \e[m"
+    echo -e " $2 ${BGreen} success \e[m "
 fi
-}
+# VALIDATE() {
+# if [ $1 -ne 0 ];
+# then
+# echo -e "$2${Red} is failure \e[m"
+# else
+# echo -e "$2${BGreen} is success \e[m"
+# fi
+# }
 
 if [ $USERID -ne 0 ]
 then
