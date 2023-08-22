7fc8f623ce0edadcb5baf442a89f6ec3a5e66a0d
psaikumar <psaikumar586@gmail.com>
2023-08-22 08:36:40 +0530

    functions

--- functions.sh
+++ functions.sh
@@ -6,10 +6,10 @@ Red='\e[1;31m'
 VALIDATE(){
 if [ $1 -ne 0 ]
 then 
-    echo -e "\eInstallation ${Red}failure \e[m "
+    echo -e"\eInstallation ${Red}failure \e[m "
     exit 1
 else
-    echo -e "\eInstallation ${BGreen}success \e[m "
+    echo -e"\eInstallation ${BGreen}success \e[m "
 fi
 }
 
