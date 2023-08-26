1afee9560ac0c8f20dedcbaedb757420fce04029
psaikumar <psaikumar586@gmail.com>
2023-08-26 15:02:53 +0530

    delete-logs

---
+++ schedule-crontab.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+# create one log file.
+#check log file exist or not with date
+#if log file date old date then delete from logs
+#create backup for daleted logfiles.
+
+MAKEDIR=/home/centos/app-log
+LOGSDIR=/home/centos/shell-script
+SCRIPTNAME=$0
+DATE=$(date +"%F-%Y-%m-%d - %H:%M:%S")
+LOGFILE=/$LOGSDIR/$DATE-$0.log
+FILE_LOC=$(find $MAKEDIR -name "*.log*" -type f -mtime +15)
+
+
+echo "$FILE_LOC"
+
+while read line
+do 
+echo "Current $DATE" &>>$LOGFILE
+rm -rf $line
+done <<< $FILE_LOC
+
+#IF [ $FILE_LOC ]
+
+
+
+
