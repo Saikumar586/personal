8211ef875e86006e44cc035fa80b873d9dfaa0ba
psaikumar <psaikumar586@gmail.com>
2023-08-28 09:24:01 +0530

    diskspace check

---
+++ diskspacegmail.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#check the disk space
+#disk space higher than 20% 
+#send a alert to user.
+
+ CURRENT=$(df / | grep / | awk '{ print $5}' | sed 's/%//g')  
+ THRESHOLD=20 
+   
+ if [ "$CURRENT" -gt "$THRESHOLD" ] ; 
+ then  
+   mail -s 'Disk Space Alert' psaikumar586@gmail.com << EOF  
+ Your root partition remaining free space is critically low. Used: $CURRENT%  
+ EOF  
+ fi
