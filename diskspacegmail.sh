05337fd6170c8d7c3a55d7eba8d9d19cb4bdff75
psaikumar <psaikumar586@gmail.com>
2023-08-29 09:19:39 +0530

    gmail sending disk space

--- diskspacegmail.sh
+++ diskspacegmail.sh
@@ -4,14 +4,57 @@
 #disk space higher than 20% 
 #send a alert to user.
 
- CURRENT=$(df / | grep / | awk '{ print $5}' | sed 's/%//g')  
- THRESHOLD=20 
- gmail=psaikumar586@gmail.com
+#  CURRENT=$(df / | grep / | awk '{ print $5}' | sed 's/%//g')  
+#  THRESHOLD=20 
+#  gmail=psaikumar586@gmail.com
    
- if [ "$CURRENT" -gt "$THRESHOLD" ] ; 
- then  
-    #mail -s 'Disk Space Alert' $gamil
-    #echo root partition remaining free space is critically low. Used: $CURRENT%   
-    echo "$CURRENT This is a test mail & Date $(date)" | mail -s "message" $gmail
+#  if [ "$CURRENT" -gt "$THRESHOLD" ] ; 
+#  then  
+#     #mail -s 'Disk Space Alert' $gamil
+#     #echo root partition remaining free space is critically low. Used: $CURRENT%   
+#     echo "$CURRENT This is a test mail & Date $(date)" | mail -s "message" $gmail
+#     fi
+
+#!/bin/bash
+
+# colors
+# validations
+# log redirections
+
+LOGFILE_DIRECTORY=/tmp
+DATE=$(date +%F:%H:%M:%S)
+SCRIPT_NAME=$0
+LOGFILE=$LOGFILE_DIRECTORY/$SCRIPT_NAME-$DATE.log
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+DISK_USAGE=$(df -hT | grep -vE 'tmpfs|Filesystem')
+DISK_USAGE_THRESHOLD=1
+message=""
+
+#IFS= means internal feild seperator is space.
+while IFS= read line
+do  
+    # this command will give you usage in number format for comparision
+    usage=$(echo $line | awk '{print $6}' | cut -d % -f1)
+    # this command will give us partition
+    partition=$(echo $line | awk '{print $1}')
+    #now you need to check whether it is more than threshold or not
+    if [ $usage -gt $DISK_USAGE_THRESHOLD ];
+    then
+        message+="HIGH DISK USAGE on $partition: $usage\n"
     fi
+done <<< $DISK_USAGE
+
+echo -e "message: $message"
+
+#echo "$message" | mail -s "High Disk usage" info@joindevops.com
+
+#how to call other shell script from your current script
+sh mail.sh psaikumasr586@gmail.com "High Disk Usage" "$message" "DEVOPS TEAM" "High Disk usage"
+
+
 
