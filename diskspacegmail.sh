b7b2565a28dae24d2e2c8897d0f2d099bef14428
psaikumar <psaikumar586@gmail.com>
2023-08-29 09:24:42 +0530

    gmail sending disk space

--- diskspacegmail.sh
+++ diskspacegmail.sh
@@ -51,7 +51,7 @@ done <<< $DISK_USAGE
 
 echo -e "message: $message"
 
-#echo "$message" | mail -s "High Disk usage" info@joindevops.com
+echo "$message" | mail -s "High Disk usage" psaikumar586@gmail.com
 
 #how to call other shell script from your current script
 #sh mail.sh psaikumasr586@gmail.com "High Disk Usage" "$message" "DEVOPS TEAM" "High Disk usage"
